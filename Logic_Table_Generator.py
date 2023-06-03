4494ef541cfe9b761750703d95ddc1762f01541f
Valerian Coelho <valerian27012004@gmail.com>
2023-06-04 00:22:40 +0530

    Nested Parenthesis Bug Fixed

--- Logic_Table_Generator.py
+++ Logic_Table_Generator.py
@@ -1,3 +1,20 @@
-expression = "(a+b)(c+d)(e+(f+g))"
-sub_equations = expression.replace(')(', ')|(').replace(') ', ')|').replace(' (', '|(').split('|')
-print(sub_equations)
+def getSubExpression(expression):
+    subExpression = []
+    count = 0
+    start = 0
+
+    for i, char in enumerate(expression):
+        if char == '(':
+            count += 1
+        elif char == ')':
+            count -= 1
+            if count == 0:
+                subExpression.append(expression[start+1:i])
+                start = i + 1
+
+    return subExpression
+
+# Example usage
+expression = "(a+b)(c+d)(e+(f+g)(h+i)+j)"
+subExpression = getSubExpression(expression)
+print(subExpression)
